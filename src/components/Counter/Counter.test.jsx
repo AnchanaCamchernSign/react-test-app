58eed158910278637e9ee39889c4ec323e0beda3
AnchanaCamchernSign <anchana.signjay@gmail.com>
2025-08-27 17:05:28 +0700

    feat: initialize React app test with Vite, add Counter and UserList components

---
+++ src/components/Counter/Counter.test.jsx
@@ -0,0 +1,23 @@
+import { beforeEach, describe, it, expect } from "vitest";
+import Counter from "./Counter";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+describe("Counter Component", () => {
+    beforeEach(() => {
+        render(<Counter />)
+    })
+
+    it("should render counter", () => {
+        expect(screen.getByText(/Counter:/)).toBeInTheDocument()
+    })
+
+    it("increments counter", () => {
+        fireEvent.click(screen.getByText("Increment"))
+        expect(screen.getByText("Counter: 1")).toBeInTheDocument()
+    })
+
+    it("decrement render counter", () => {
+        fireEvent.click(screen.getByText("Decrement"))
+        expect(screen.getByText("Counter: -1")).toBeInTheDocument()
+    })
+})
