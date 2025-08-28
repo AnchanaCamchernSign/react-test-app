8eed2cf8332752ee8ce17d473d1952bd9979345e
AnchanaCamchernSign <anchana.signjay@gmail.com>
2025-08-28 10:41:43 +0700

    test: Add unit test for search users

--- src/components/UserList/UserList.test.jsx
+++ src/components/UserList/UserList.test.jsx
@@ -1,11 +1,11 @@
-import { render, waitFor, screen } from "@testing-library/react";
+import { render, waitFor, screen, fireEvent } from "@testing-library/react";
 import axios from "axios";
 import { describe, expect, vi, it } from "vitest";
 import UserList from "./UserList";
 
 vi.mock('axios')
 
-describe('UserList component', () => { 
+describe('UserList component', () => {
     const mockUsers = [
         {
             id: '1',
@@ -36,4 +36,19 @@ describe('UserList component', () => {
             expect(screen.getByText('minie@gmail.com')).toBeInTheDocument()
         })
     })
+
+    it('filters users based on search input', async () => {
+        axios.get.mockResolvedValue({ data: mockUsers })
+        render(<UserList />)
+
+        await waitFor(() => {
+            fireEvent.change(screen.getByPlaceholderText('Search by name or email'), {
+                target: { value: 'Jay W Youth'},
+            })
+            expect(screen.getByText('Jay W Youth')).toBeInTheDocument()
+            expect(screen.getByText('callmejay@gmail.com')).toBeInTheDocument()
+        })
+    })
+
+    it('handle API failure without problems and still renders')
 })
